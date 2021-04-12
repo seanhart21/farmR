cfe0b7d90a1bc45c6becbfd5f93340d6df115bf0
Curley <curley@utexas.edu>
2021-04-12 11:52:01 -0500

    added cleaning squirrels

--- code/cleaning_raw_data.R
+++ code/cleaning_raw_data.R
@@ -2,3 +2,31 @@
 library(tidyverse)
 df <- readxl::read_xlsx("raw_data/Hart Farm Ag Log Excerpt.xlsx")
 head(df)
+
+
+## Get Dates from excel format.
+
+df[,1]
+tail(df[,1])
+
+dates <- as.data.frame(df[,1])
+dates <- as.numeric(dates[1:nrow(dates),])
+
+library(lubridate)
+as.Date('1899-12-30') + days(dates)
+df$date <- as.Date('1899-12-30') + days(dates)
+head(df)
+
+## just get squirrels
+squirrel.id <- which(df[3,1:56]=="Squirrel") #13
+
+sq <- df %>% select(date, n = squirrel.id)
+sq <- sq[4:nrow(sq),]
+
+head(sq)
+sq$n <- ifelse(is.na(sq$n), 0, as.numeric(sq$n))
+
+head(sq)
+
+ggplot(sq, aes(x=date,y=n)) + geom_point()+
+  theme_classic()
